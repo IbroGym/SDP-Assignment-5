a5015db2dd3a368f98a1d354adec7f9e502e2557
IbroGym <galymzhanulyibragim@gmail.com>
2024-10-24 17:02:47 +0500

    Second Commit

--- src/CalculatorController.java
+++ src/CalculatorController.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class CalculatorController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalculatorController {
+    private final CalculatorView view;
+    private final CalculatorModel model;
+
+    public CalculatorController(CalculatorView view, CalculatorModel model) {
+        this.view = view;
+        this.model = model;
+
+        this.view.addOperationListener(new OperationListener());
+    }
+
+    class OperationListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                double firstNum = view.getFirstNumber();
+                double secondNum = view.getSecondNumber();
+                String result = "";
+
+                switch (view.getOperationCommand(e)) {
+                    case "+":
+                        result = String.valueOf(model.add(firstNum, secondNum));
+                        break;
+                    case "-":
+                        result = String.valueOf(model.subtract(firstNum, secondNum));
+                        break;
+                    case "*":
+                        result = String.valueOf(model.multiply(firstNum, secondNum));
+                        break;
+                    case "/":
+                        try {
+                            result = String.valueOf(model.divide(firstNum, secondNum));
+                        } catch (ArithmeticException ex) {
+                            result = "Ошибка: деление на ноль";
+                        }
+                        break;
+                }
+                view.setResult(result);
+            } catch (NumberFormatException ex) {
+                view.displayErrorMessage("Ошибка: введите корректные числа");
+            }
+        }
+    }
 }
--- src/CalculatorView.java
+++ src/CalculatorView.java
@@ -1,2 +1,62 @@
-package PACKAGE_NAME;public class CalculatorView {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalculatorView extends JFrame {
+    private final JTextField firstNumber = new JTextField(10);
+    private final JTextField secondNumber = new JTextField(10);
+    private final JButton addButton = new JButton("+");
+    private final JButton subtractButton = new JButton("-");
+    private final JButton multiplyButton = new JButton("*");
+    private final JButton divideButton = new JButton("/");
+    private final JTextField resultField = new JTextField(15);
+
+    public CalculatorView() {
+        JPanel calcPanel = new JPanel();
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(400, 200);
+
+        calcPanel.add(new JLabel("Первое число:"));
+        calcPanel.add(firstNumber);
+        calcPanel.add(new JLabel("Второе число:"));
+        calcPanel.add(secondNumber);
+
+        calcPanel.add(addButton);
+        calcPanel.add(subtractButton);
+        calcPanel.add(multiplyButton);
+        calcPanel.add(divideButton);
+
+        calcPanel.add(new JLabel("Результат:"));
+        calcPanel.add(resultField);
+
+        this.add(calcPanel);
+    }
+
+    public double getFirstNumber() {
+        return Double.parseDouble(firstNumber.getText());
+    }
+
+    public double getSecondNumber() {
+        return Double.parseDouble(secondNumber.getText());
+    }
+
+    public void setResult(String result) {
+        resultField.setText(result);
+    }
+
+    public void addOperationListener(ActionListener listenForCalcButton) {
+        addButton.addActionListener(listenForCalcButton);
+        subtractButton.addActionListener(listenForCalcButton);
+        multiplyButton.addActionListener(listenForCalcButton);
+        divideButton.addActionListener(listenForCalcButton);
+    }
+
+    public String getOperationCommand(ActionEvent e) {
+        return e.getActionCommand();
+    }
+
+    public void displayErrorMessage(String errorMessage) {
+        JOptionPane.showMessageDialog(this, errorMessage);
+    }
 }
